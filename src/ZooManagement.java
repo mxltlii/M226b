af77295a0c2e70dfb71b5b1e347bc3026e52bf04
LiamSMD <62339584+LiamSMD@users.noreply.github.com>
2021-01-25 14:04:32 +0100

    Added Overseer as 3rd Keeper & Commented ZooManagement

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,13 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
+// Im ZooManagement werden administrative Dinge getan wie z.B. die Dinos laden und in ihre Gehege einsperren.
 public class ZooManagement {
     public static ArrayList<Cage> LoadDinos() {
         try {
-            ArrayList<String> arrList = new ArrayList<>();
+            Scanner sc = new Scanner(new File("C:\\Users\\Liam\\Desktop\\Dinodata.txt"));
+            sc.useDelimiter(",");
 
+            ArrayList<String> arrList = new ArrayList<>();
+            // Platzhalter-Arraylists werden erstellt
             ArrayList<Dinosaur> rexList = new ArrayList<>();
             ArrayList<Dinosaur> raptorList = new ArrayList<>();
             ArrayList<Dinosaur> brontoList = new ArrayList<>();
@@ -15,11 +21,11 @@ public class ZooManagement {
             ArrayList<Dinosaur> diloList = new ArrayList<>();
             ArrayList<ArrayList<Dinosaur>> allDinoList = new ArrayList<>();
 
-            Scanner sc = new Scanner(new File("C:\\Users\\Liam\\Desktop\\Dinodata.txt"));
-            sc.useDelimiter(",");
+            // Das Text-File wird gelesen
             while (sc.hasNext()) {
                 arrList.add(sc.next());
             }
+            // Die Daten werden je nach Dino-Art in unterschiedliche Arrays aufgeteilt
             for (int i = 0; i < arrList.size() - 1; i += 3) {
                 switch (arrList.get(i)) {
                     case "T-Rex":
@@ -39,28 +45,35 @@ public class ZooManagement {
                         break;
                 }
             }
+
+            // Die verschiedenen Dino-Listen werden der Gesamtliste hinzugefügt
             allDinoList.add(rexList);
             allDinoList.add(raptorList);
             allDinoList.add(brontoList);
             allDinoList.add(trikeList);
             allDinoList.add(diloList);
 
+            // Gehege und Wärter werden erstellt
             ArrayList<Cage> cageList = new ArrayList<>();
-            Feeder feeder = new Feeder("Peter Geizer", "Fütterer");
+            ArrayList<Keeper> keeperList  = new ArrayList<>();
+            keeperList.add(new Feeder("Peter Geizer", "Fütterer"));
+            keeperList.add(new DinoDoctor("", "Fütterer"));
+            keeperList.add(new Overseer("Bledon Asani", "Fütterer"));
+
+
+            //Zufällige Wärter werden für jedes Gehege ausgwählt
+            Random r = new Random();
 
             for (ArrayList<Dinosaur> dinoCageList : allDinoList) {
-                cageList.add(new Cage(dinoCageList, feeder));
+                int random = r.nextInt(3);
+                cageList.add(new Cage(dinoCageList, keeperList.get(random)));
             }
 
-            Zoo zoo = new Zoo();
-            zoo.Start(cageList);
             return cageList;
 
         } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
+            System.out.println("File nicht gefunden");
             e.printStackTrace();
-
-
         }
         return null;
     }
