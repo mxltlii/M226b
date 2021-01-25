a579c55d5a2167c43fc6b2532b437a454dc84523
LiamSMD <62339584+LiamSMD@users.noreply.github.com>
2021-01-25 13:50:00 +0100

    Finished DinoDoctor-Classs so far

--- src/DinoDoctor.java
+++ src/DinoDoctor.java
@@ -1,11 +1,11 @@
-public class DinoDoctor extends Keeper implements IDoJob{
+public class DinoDoctor extends Keeper implements IDoJob {
 
     @Override
     public String DoJob(String dinospecies) {
-        return "Der Doktor geht zum " + dinospecies + " und gibt ihm eine Spritze.";
+        return "Der Doktor geht einem " + dinospecies + " und gibt ihm eine Spritze.";
     }
 
-    public DinoDoctor(String name, String role){
+    public DinoDoctor(String name, String role) {
         super(name, role);
 
     }
