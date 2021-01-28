13d19645787b3dcd064e8884a209b9345df00505
mxltlii <merttakil@hotmail.com>
2021-01-28 20:35:54 +0100

    Constructor für Visitor erstellt. getBalance erstellt

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,11 @@
 public class Visitor {
     private float balance = 120.00f;
+
+    public Visitor() {
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return this.balance;
+    }
 }
