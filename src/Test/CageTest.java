ae4f0ff4c56770fa44c44b32e0fcb00b6c04c1ad
mxltlii <merttakil@hotmail.com>
2021-01-29 18:44:32 +0100

    Test Files erstellt.

---
+++ src/Test/CageTest.java
@@ -0,0 +1,28 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CageTest {
+
+    @Test
+    void watchDinos() {
+    }
+
+    @Test
+    void watchKeeper() {
+    }
+
+    @Test
+    void getSpecies() {
+    }
+
+    @Test
+    void beFed() {
+    }
+
+    @Test
+    void getDinoList() {
+    }
+}
---
+++ src/Test/ZooTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZooTest {
+
+    @Test
+    void start() {
+
+    }
+}
